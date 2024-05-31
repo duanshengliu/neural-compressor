08bcdb36de931c9002d5bbb20a526111496d3554
Mengni Wang <mengni.wang@intel.com>
2024-05-31 12:05:15 +0300

    update script
    Signed-off-by: Mengni Wang <mengni.wang@intel.com>

--- examples/3.x_api/pytorch/nlp/huggingface_models/language-modeling/quantization/mx/run_clm_no_trainer.py
+++ examples/3.x_api/pytorch/nlp/huggingface_models/language-modeling/quantization/mx/run_clm_no_trainer.py
@@ -15,18 +15,6 @@ if "hpu" in device:
     htcore.hpu_set_env()
     torch.device('hpu')
 
-    def show_msg():
-        print("Number of HPU graphs:", len(glob.glob(".graph_dumps/*PreGraph*")))
-        mem_stats = memory_stats()
-        mem_dict = {
-            "memory_allocated (GB)": np.round(mem_stats["InUse"] / 1024**3, 2),
-            "max_memory_allocated (GB)": np.round(mem_stats["MaxInUse"] / 1024**3, 2),
-            "total_memory_available (GB)": np.round(mem_stats["Limit"] / 1024**3, 2),
-        }
-        for k, v in mem_dict.items():
-            print("{:35} = {} GB".format(k[:-5].replace("_", " ").capitalize(), v))
-
-
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--model", nargs="?", default="EleutherAI/gpt-j-6b"
@@ -38,6 +26,8 @@ parser.add_argument(
     "--revision", default=None,
     help="Transformers parameter: set the model hub commit number")
 parser.add_argument("--quantize", action="store_true")
+parser.add_argument('--buckets', type=int, nargs='+', \
+                    help="Input length buckets to use with static_shapes", default=[256, 512])
 # dynamic only now
 parser.add_argument("--w_dtype", type=str, default="int8", 
                     choices=["int8", "int4", "int2", "fp8_e5m2", "fp8_e4m3", "fp6_e3m2", 
@@ -96,8 +86,7 @@ def eval_func(user_model, tokenizer, args):
             self.tokenizer = tokenizer
             self.model = model.eval()
             self._batch_size = args.batch_size
-            self.buckets = [256, 512]
-            #self.buckets = list(sorted(args.buckets))
+            self.buckets = args.buckets
             self.options = options
             self._device = "hpu"
             torch.set_grad_enabled(False)
@@ -207,59 +196,41 @@ if "hpu" in device:
         # inference optimization
         import habana_frameworks.torch.hpu.graphs as htgraphs
         user_model = htgraphs.wrap_in_hpu_graph(user_model)
-    show_msg()
 
 if args.accuracy:
-    #eval_func(user_model, tokenizer=tokenizer, args=args)
+    eval_func(user_model, tokenizer=tokenizer, args=args)
     #import pdb;pdb.set_trace()
-    from intel_extension_for_transformers.transformers.llm.evaluation.lm_eval import evaluate, LMEvalParser
-    eval_args = LMEvalParser(
-        model="hf",
-        user_model=user_model,
-        tokenizer=tokenizer,
-        batch_size=args.batch_size,
-        tasks=','.join(args.tasks),
-        device=device,
-        limit=10,
-    )
-    results = evaluate(eval_args)
-    dumped = json.dumps(results, indent=2)
-    if args.save_accuracy_path:
-        with open(args.save_accuracy_path, "w") as f:
-            f.write(dumped)
+    #from intel_extension_for_transformers.transformers.llm.evaluation.lm_eval import evaluate, LMEvalParser
+    #eval_args = LMEvalParser(
+    #    model="hf",
+    #    user_model=user_model,
+    #    tokenizer=tokenizer,
+    #    batch_size=args.batch_size,
+    #    tasks=','.join(args.tasks),
+    #    device=device,
+    #    limit=10,
+    #)
+    #results = evaluate(eval_args)
+    #dumped = json.dumps(results, indent=2)
+    #if args.save_accuracy_path:
+    #    with open(args.save_accuracy_path, "w") as f:
+    #        f.write(dumped)
 
-    eval_acc = 0
-    for task_name in args.tasks:
-        if task_name == "wikitext":
-            print("Accuracy for %s is: %s" %
-                  (task_name, results["results"][task_name]["word_perplexity,none"]))
-            eval_acc += results["results"][task_name]["word_perplexity,none"]
-        else:
-            print("Accuracy for %s is: %s" %
-                  (task_name, results["results"][task_name]["acc,none"]))
-            eval_acc += results["results"][task_name]["acc,none"]
+    #eval_acc = 0
+    #for task_name in args.tasks:
+    #    if task_name == "wikitext":
+    #        print("Accuracy for %s is: %s" %
+    #              (task_name, results["results"][task_name]["word_perplexity,none"]))
+    #        eval_acc += results["results"][task_name]["word_perplexity,none"]
+    #    else:
+    #        print("Accuracy for %s is: %s" %
+    #              (task_name, results["results"][task_name]["acc,none"]))
+    #        eval_acc += results["results"][task_name]["acc,none"]
 
 if args.performance:
-    user_model.eval()
-    from intel_extension_for_transformers.llm.evaluation.lm_eval import evaluate
-    import time
-    samples = args.iters * args.batch_size
-    start = time.time()
-    results = evaluate(
-        model="hf",
-        tokenizer=tokenizer,
-        user_model=user_model,
-        batch_size=args.batch_size,
-        tasks=','.join(args.tasks),
-        limit=samples,
-    )
-    end = time.time()
-    for task_name in args.tasks:
-        if task_name == "wikitext":
-            acc = results["results"][task_name]["word_perplexity"]
-        else:
-            acc = results["results"][task_name]["acc"]
-    print("Accuracy: %.5f" % acc)
-    print('Throughput: %.3f samples/sec' % (samples / (end - start)))
-    print('Latency: %.3f ms' % ((end - start)*1000 / samples))
-    print('Batch size = %d' % args.batch_size)
+    eval_start = time.perf_counter()
+    input_prompt = "Intel is a company which"
+    input_tokens = torch.ones((1, 128), dtype=torch.long).to('hpu')
+    generation_config = {"min_new_tokens": 100, "max_new_tokens": 100}
+    outputs = user_model.generate(input_tokens, **generation_config)
+    print("Duration of generating 100 tokens :", time.perf_counter() - eval_start)
